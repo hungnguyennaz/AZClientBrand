1d113d939012d05cccaa1ed5988aed70146079b3
Hung Nguyen <101444336+hungnguyennaz@users.noreply.github.com>
2022-10-29 17:08:38 +0700

    u

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -8,9 +8,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientBrandRetriever.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method= "getClientModName", cancellable=true, remap=false)
-      private void getClientModName(CallbackInfoReturnable<String> callback) {
-      callback.setReturnValue("AZClient");
-	}
+public abstract class ExampleMixin {
+    @Inject(at = @At("HEAD"), method = "getClientModName", cancellable = true, remap = false)
+    private static void getClientModName(CallbackInfoReturnable<String> callback) {
+        callback.setReturnValue("AZClient");
+        }
+    }
 }
